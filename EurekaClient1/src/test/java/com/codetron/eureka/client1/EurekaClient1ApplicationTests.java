014dba390362991ec52321b5c40012e0f49c823d
yo <josete@localhost.localdomain>
2016-03-11 20:05:15 +0100

    Initial setup

---
+++ EurekaClient1/src/test/java/com/codetron/eureka/client1/EurekaClient1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codetron.eureka.client1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EurekaClient1Application.class)
+public class EurekaClient1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
