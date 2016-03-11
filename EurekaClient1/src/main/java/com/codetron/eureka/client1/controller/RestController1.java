7f52407c0ce96ec9b0f1f0e23fbf599611683377
josetesan <muallin@gmail.com>
2016-03-11 21:17:28 +0100

    Added docker-compose

---
+++ EurekaClient1/src/main/java/com/codetron/eureka/client1/controller/RestController1.java
@@ -0,0 +1,7 @@
+package com.codetron.eureka.client1.controller;
+
+/**
+ * Created by josete on 11/03/16.
+ */
+public class RestController1 {
+}
---
+++ EurekaClient2/src/main/java/com/codetron/eureka/client2/controller/RestController1.java
@@ -0,0 +1,7 @@
+package com.codetron.eureka.client2.controller;
+
+/**
+ * Created by josete on 11/03/16.
+ */
+public class RestController2 {
+}
