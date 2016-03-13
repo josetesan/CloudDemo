0b1dbdbd833c043df6de370ad6a8d535942bd144
josetesan <muallin@gmail.com>
2016-03-13 11:52:30 +0100

    Added hystrix dashboard and circuit breaker

--- EurekaClient1/src/main/java/com/codetron/eureka/client1/EurekaClient1Application.java
+++ EurekaClient1/src/main/java/com/codetron/eureka/client1/EurekaClient1Application.java
@@ -2,10 +2,12 @@ package com.codetron.eureka.client1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class EurekaClient1Application {
 
 	public static void main(String[] args) {
--- EurekaClient1/src/main/java/com/codetron/eureka/client1/controller/RestController1.java
+++ EurekaClient1/src/main/java/com/codetron/eureka/client1/controller/RestController1.java
@@ -1,5 +1,6 @@
 package com.codetron.eureka.client1.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +21,14 @@ public class RestController1 {
     private RestTemplate restTemplate;
 
     @RequestMapping(method = RequestMethod.GET, value = "/client1")
+    @HystrixCommand(fallbackMethod = "sayHello")
     public ResponseEntity<?> retrieveData() {
         String response = restTemplate.getForObject("http://eureka-client2/hello",String.class);
         return ResponseEntity.ok(response);
+    }
+
+
+    private ResponseEntity<?> sayHello() {
+        return ResponseEntity.ok("Hello from fallback method");
     }
 }
