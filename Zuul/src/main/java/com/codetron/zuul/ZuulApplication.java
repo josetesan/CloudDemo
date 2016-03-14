42d8b92b5cf482beb204c2236f35502210342907
josetesan <muallin@gmail.com>
2016-03-14 21:25:14 +0100

    Typo in Zuul

--- Zuul/src/main/java/com/codetron/zuul/ZuulApplication.java
+++ Zuul/src/main/java/com/codetron/zuul/ZuulApplication.java
@@ -2,7 +2,6 @@ package com.codetron.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
