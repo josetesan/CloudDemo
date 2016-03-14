26449d4170d00d8a989c2b2392df8b592396dea8
josetesan <muallin@gmail.com>
2016-03-14 22:21:04 +0100

    Client2 displays running port, so it can check loadbalancing

--- EurekaClient2/src/main/java/com/codetron/eureka/client2/controller/RestController2.java
+++ EurekaClient2/src/main/java/com/codetron/eureka/client2/controller/RestController2.java
@@ -1,17 +1,24 @@
 package com.codetron.eureka.client2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 /**
  * Created by josete on 11/03/16.
  */
 @RestController
-public class RestController2 {
+public class RestController2 implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+
+    private Integer runningPort;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -19,7 +26,14 @@ public class RestController2 {
 
     @RequestMapping(method = RequestMethod.GET, value = "/hello")
     public ResponseEntity<?> retrieveData() {
-        return ResponseEntity.ok("Hello from client2");
+        final String response = String.format("Hello from client2 at port %s",runningPort);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @Override
+    public void onApplicationEvent(final EmbeddedServletContainerInitializedEvent event) {
+        runningPort = event.getEmbeddedServletContainer().getPort();
     }
 
 
