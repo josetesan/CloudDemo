1413e42907defe92991b5d9d15859109f2c97068
jose luis sanchez <muallin@gmail.com>
2016-03-15 21:28:19 +0100

    Should work with host network based
    Upgraded to Brixton.RC1

--- EurekaClient1/src/main/java/com/codetron/eureka/client1/EurekaClient1Application.java
+++ EurekaClient1/src/main/java/com/codetron/eureka/client1/EurekaClient1Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +15,11 @@ public class EurekaClient1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClient1Application.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
